4f41bf4f4bdde62af2e3a3076f0b53055a5cf41c
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-05-11 18:37:09 +0300

    2

--- gallery.js
+++ gallery.js
@@ -1,6 +1,12 @@
 import images from './gallery-items.js'
 
 const galleryConteiner = document.querySelector(".js-gallery");
+const lightbox = document.querySelector(".js-lightbox");
+const overlay = document.querySelector(".lightbox__overlay");
+const lightboxImg = document.querySelector(".lightbox__image");
+const lightboxCloseBtn = document.querySelector(
+  '[data-action="close-lightbox"]'
+);
 
 
 function galleryListMarkupCreate(images) {
@@ -27,7 +33,7 @@ function galleryListMarkupCreate(images) {
 const imgMarkupCreate = galleryListMarkupCreate(images);
 
 galleryConteiner.insertAdjacentHTML('beforeend', imgMarkupCreate);
-
+lightboxCloseBtn.addEventListener('click', onCloseModal);
 
 galleryConteiner.addEventListener('click', onGalleryConteinerClick)
 
@@ -38,5 +44,13 @@ function onGalleryConteinerClick(evt) {
   if (!isSwathEl) {
     return;
   }
- 
+  const imageSource = evt.target.dataset.source;
+
+  lightboxImg.src = imageSource;
+  lightbox.classList.add('is-open');   
+}
+
+function onCloseModal() {
+  lightboxImg.src = '';
+  lightbox.classList.remove('is-open');    
 }
